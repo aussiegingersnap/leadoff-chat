27ef3c31fc0cdee3b5537f6f6e672bef5afbf800
Jason Toevs <66174470+aussiegingersnap@users.noreply.github.com>
2023-10-26 16:41:42 -0500

    branded fix

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -54,7 +54,7 @@ export const Badge = (props: Props) => {
     }}>Powered by
       <a
         ref={liteBadge}
-        href={'https://twitter.com/JasonToevs}
+        href={'https://twitter.com/JasonToevs'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
